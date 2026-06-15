06701326be641a8e7dae996e2c7472b6708b2d42
Nicholas Trimble <nicktrimble07@gmail.com>
2026-06-14 20:56:02 -0500

    Pushing one last time

--- application.py
+++ application.py
@@ -3,6 +3,8 @@ import sys
 
 # Ensure execution paths align
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+# Add the backend directory explicitly for Azure's runtime engine
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'backend')))
 
 try:
     from backend.main import app
